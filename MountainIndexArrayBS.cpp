e5face25fbbd59aec2cb50a33b0a725a53a4fb78
Kartik Attri <attrikartik494@gmail.com>
2025-02-08 10:56:00 +0530

    Sorted Array and Mountain Array index

---
+++ MountainIndexArrayBS.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+    int PeakIndexMountainArray(vector<int>& A){
+        int st=0 , end= A.size()-1;
+
+        while (st<=end)
+        {
+            int mid = st + (end-st)/2;
+
+            if (A[mid-1]< A[mid] && A[mid]>A[mid+1]){
+
+                return mid;
+            }
+            
+            else if( A[mid-1] < A[mid]){
+                st=mid+1;
+            }
+
+            else{
+                end=mid-1;
+            }
+
+            
+        }
+
+
+        return -1;
+        
+    }
